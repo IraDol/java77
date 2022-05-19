ec533d46eb985afa133ff0f42fb2e5688c58a450
Irina Dolgova <claycharm@yandex.ru>
2022-05-19 16:49:47 +0300

    add new test

--- src/test/java/ru/netology/statistic/StatisticsServiceTest.java
+++ src/test/java/ru/netology/statistic/StatisticsServiceTest.java
@@ -1,6 +1,7 @@
 package ru.netology.statistic;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -15,6 +16,16 @@ public class StatisticsServiceTest {
 
         long actual = service.findMax(incomesInBillions);
 
+        assertEquals(expected, actual);
+    }
+    @Test
+    void findMaxSameIncome() {
+        StatisticsService service = new StatisticsService();
+
+        long[] incomesInBillions = {4, 5, 8, 12, 5, 3, 8, 6, 11, 11, 12};
+        long expected = 12;
+
+        long actual = service.findMax(incomesInBillions);
         assertEquals(expected, actual);
     }
 }
